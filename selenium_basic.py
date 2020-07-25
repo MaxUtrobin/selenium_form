c9c435375eb726a3da9657209edec165831d9e51
MaxUtrobin <ma.utrobin@gmail.com>
2020-07-25 11:28:58 +0300

    HW2

--- selenium_basic.py
+++ selenium_basic.py
@@ -1,27 +1,27 @@
-# https://www.seleniumeasy.com/test/basic-first-form-demo.html
-# Add case for Two Input Fields block
-
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-
 TEST_DATA = {
         'single_input': 'My best message here!!',
         'two_input_first': 333,
         'two_input_second': 444
     }
-
-
-
 driver = webdriver.Chrome()
 driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html")
-
+time.sleep(2)
 if driver.find_element(By.ID, "at-cv-lightbox-close").size != 0:
     driver.find_element(By.ID, "at-cv-lightbox-close").click()
-
-driver.find_element(By.CSS_SELECTOR, "input#user-message").send_keys(TEST_DATA['single_input'])
-driver.find_element(By.CSS_SELECTOR, "form#get-input button").click()
-
-assert driver.find_element(By.ID, "display").text == TEST_DATA['single_input']
+time.sleep(2)
+driver.find_element_by_css_selector("input#user-message").send_keys(TEST_DATA['single_input'])
+time.sleep(2)
+driver.find_element_by_css_selector("#get-input > button").click()
+time.sleep(2)
+driver.find_element_by_css_selector("#sum1").send_keys(TEST_DATA['two_input_first'])
+time.sleep(2)
+driver.find_element_by_css_selector("#sum2").send_keys(TEST_DATA['two_input_second'])
+time.sleep(2)
+driver.find_element_by_css_selector("#gettotal > button").click()
+time.sleep(2)
+assert driver.find_element_by_css_selector("#displayvalue").text == '777'
+time.sleep(2)
 driver.quit()
--- selenium_form.py
+++ selenium_form.py
@@ -1,6 +1,62 @@
 # Open https://the-internet.herokuapp.com/login
-
 # Please automate next test cases:
 # 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
 # 2. Login with invalid creds and check validation error
 # 3. Logout from app and assert you successfully logged out
+# ----------------------------------------------------------------------------------------------------------
+import time
+from selenium import webdriver
+# ----------------------------------------------------------------------------------------------------------
+# 1. Login with valid creds (tomsmith/SuperSecretPassword!) and assert you successfully logged in
+# ----------------------------------------------------------------------------------------------------------
+TEST_DATA = {
+        'username': 'tomsmith',
+        'password': 'SuperSecretPassword!',
+    }
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+time.sleep(2)
+driver.find_element_by_css_selector("#username").send_keys(TEST_DATA['username'])
+driver.find_element_by_css_selector("#password").send_keys(TEST_DATA['password'])
+driver.find_element_by_css_selector("#login > button > i").click()
+elem = driver.find_element_by_css_selector('[id="flash"]').text
+assert 'You logged into a secure area!' in elem
+time.sleep(2)
+driver.quit()
+# ----------------------------------------------------------------------------------------------------------
+# 2. Login with invalid creds and check validation error
+# ----------------------------------------------------------------------------------------------------------
+TEST_DATA = {
+        'username': '123123123',
+        'password': '123123132',
+    }
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+time.sleep(2)
+driver.find_element_by_css_selector("#username").send_keys(TEST_DATA['username'])
+driver.find_element_by_css_selector("#password").send_keys(TEST_DATA['password'])
+driver.find_element_by_css_selector("#login > button > i").click()
+elem = driver.find_element_by_css_selector('[id="flash"]').text
+assert 'Your username is invalid!' in elem
+time.sleep(2)
+driver.quit()
+# ----------------------------------------------------------------------------------------------------------
+# 3. Logout from app and assert you successfully logged out
+# ----------------------------------------------------------------------------------------------------------
+TEST_DATA = {
+        'username': 'tomsmith',
+        'password': 'SuperSecretPassword!',
+    }
+driver = webdriver.Chrome()
+driver.get("https://the-internet.herokuapp.com/login")
+time.sleep(2)
+driver.find_element_by_css_selector("#username").send_keys(TEST_DATA['username'])
+driver.find_element_by_css_selector("#password").send_keys(TEST_DATA['password'])
+driver.find_element_by_css_selector("#login > button > i").click()
+time.sleep(2)
+driver.find_element_by_css_selector("#content > div > a > i").click()
+time.sleep(2)
+elem = driver.find_element_by_css_selector('[id="flash"]').text
+assert 'You logged out of the secure area!' in elem
+time.sleep(2)
+driver.quit()
